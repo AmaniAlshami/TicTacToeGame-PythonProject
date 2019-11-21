9cdee9896830d58787e405630918b65f356ae101
RahebahAli <371202993@qu.edu.sa>
2019-11-21 04:37:32 -0800

    add convert between x and o

--- TicTacToe.py
+++ TicTacToe.py
@@ -4,9 +4,20 @@ top = tkinter.Tk()
 top.title("Tic Tac Toc")
 # use to store marker
 board= [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']
-def place_marker(position,marker,button):
- board[position] = marker 
- button.configure(text=marker)
+def place_marker(position,button):
+    global player1
+    if button["text"]=="" and  player1==True:
+        marker="X"
+        if win_check(board,"X"):
+            print("win")
+        player1=False
+    elif button["text"]=="" and player1==False:
+        marker="O"
+        if win_check(board,"O"):
+            print("win")
+        player1=True
+    board[position] = marker 
+    button.configure(text=marker)
 
 
 def win_check(board, mark):
@@ -23,25 +34,24 @@ def win_check(board, mark):
    
 
 # Buttons
-but0 = tkinter.Button(top,text="1" , width=10, height=5, command=lambda:place_marker(1,"X",but0))
-but0.grid(row=1,column=0)
-but1 = tkinter.Button(top,text="2" , width=10, height=5, command=lambda:place_marker(2,"O",but1))
+but0 = tkinter.Button(top,text="" , width=10, height=5, command=lambda:place_marker(1,but0))
+but0.grid(row=1,column=0) 
+but1 = tkinter.Button(top,text="" , width=10, height=5, command=lambda:place_marker(2,but1))
 but1.grid(row=1,column=1)
-but2 = tkinter.Button(top,text="3" , width=10, height=5)
+but2 = tkinter.Button(top,text="" , width=10, height=5, command=lambda:place_marker(3,but2))
 but2.grid(row=1,column=2)
-but3 = tkinter.Button(top,text="4" , width=10, height=5)
+but3 = tkinter.Button(top,text="" , width=10, height=5, command=lambda:place_marker(4,but3))
 but3.grid(row=2,column=0)
-but4 = tkinter.Button(top,text="5" , width=10, height=5)
+but4 = tkinter.Button(top,text="" , width=10, height=5, command=lambda:place_marker(5,but4))
 but4.grid(row=2,column=1)
-but5 = tkinter.Button(top,text="6" , width=10, height=5)
+but5 = tkinter.Button(top,text="" , width=10, height=5, command=lambda:place_marker(6,but5))
 but5.grid(row=2,column=2)
-but6 = tkinter.Button(top,text="7" , width=10, height=5)
+but6 = tkinter.Button(top,text="" , width=10, height=5, command=lambda:place_marker(7,but6))
 but6.grid(row=3,column=0)
-but7 = tkinter.Button(top,text="8" , width=10, height=5)
+but7 = tkinter.Button(top,text="" , width=10, height=5, command=lambda:place_marker(8,but7))
 but7.grid(row=3,column=1)
-but8 = tkinter.Button(top,text="9" , width=10, height=5)
+but8 = tkinter.Button(top,text="" , width=10, height=5, command=lambda:place_marker(9,but8))
 but8.grid(row=3,column=2)
-
 
 
 
